acef3ccc9e4d23326c0f175d4c211938de3a2ac8
Naoufal-123 <nbenaboud70@gmail.com>
2023-07-08 12:49:17 -0700

    koc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the name of the program.
+ * @argc: Number of arguments
+ * @argv: Arguments vlue
+ * Auth: Mouhcine Ezzaidi
+ *
+ * Return: Alway 0 (sucess)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
+
